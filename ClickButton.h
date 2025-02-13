e8aa7e8459384e4682a3e5071b0b7d392a44c13b
Nezaemmy <38810350+Nezaemmy@users.noreply.github.com>
2025-02-13 15:50:04 +0100

    Update ClickButton.h

--- ClickButton.h
+++ ClickButton.h
@@ -20,9 +20,9 @@ class ClickButton
     void Update();
     int clicks;                   // button click counts to return
     boolean depressed;            // the currently debounced button (press) state (presumably it is not sad :)
-    long debounceTime;
-    long multiclickTime;
-    long longClickTime;
+    uint64_t debounceTime;
+    uint64_t multiclickTime;
+    uint64_t longClickTime;
     boolean changed;
   private:
     uint8_t _pin;                 // Arduino pin connected to the button
@@ -30,7 +30,7 @@ class ClickButton
     boolean _btnState;            // Current appearant button state
     boolean _lastState;           // previous button reading
     int _clickCount;              // Number of button clicks within multiclickTime milliseconds
-    long _lastBounceTime;         // the last time the button input pin was toggled, due to noise or a press
+    uint64_t _lastBounceTime;         // the last time the button input pin was toggled, due to noise or a press
 };
 
 #endif
